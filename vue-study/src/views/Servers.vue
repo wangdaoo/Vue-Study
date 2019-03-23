575d12cc8ed4db68539aab00c3f65a22c055a962
MARS <wangyunfan@vtstar.net>
2019-03-23 12:45:06 +0800

    add vue-cli

---
+++ vue-study/src/views/Servers.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="servers">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    el: 'servers',
+    data: {
+        msg: 'Hello Mars'
+    }
+}
+</script>
